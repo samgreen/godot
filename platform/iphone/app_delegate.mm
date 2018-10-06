9af616bff8a0b3812052881ff25ae27c4b280ade
Sam Green <samuel.green@instacart.com>
2018-10-06 05:57:17 -0700

    Separate gamepad management to a new class

--- platform/iphone/app_delegate.mm
+++ platform/iphone/app_delegate.mm
@@ -29,10 +29,10 @@
 /*************************************************************************/
 
 #import "app_delegate.h"
+#import "gamepad_iphone.h"
 #import "gl_view.h"
 #import "view_controller.h"
 #import <CoreMotion/CoreMotion.h>
-#import <GameController/GameController.h>
 
 #import "core/project_settings.h"
 #import "drivers/coreaudio/audio_driver_coreaudio.h"
@@ -71,264 +71,6 @@ extern void iphone_finish();
 
 CMMotionManager *motionManager;
 bool motionInitialised;
-
-NSMutableDictionary *ios_joysticks = nil;
-NSMutableArray *pending_ios_joysticks = nil;
-
-- (GCControllerPlayerIndex)getFreePlayerIndex {
-	return (GCControllerPlayerIndex)ios_joysticks.count - 1;
-};
-
-void _ios_add_joystick(GCController *controller, AppDelegate *delegate) {
-	// get a new id for our controller
-	int joy_id = OSIPhone::get_singleton()->get_unused_joy_id();
-	if (joy_id != -1) {
-		// assign our player index
-		if (controller.playerIndex == GCControllerPlayerIndexUnset) {
-			controller.playerIndex = [delegate getFreePlayerIndex];
-		};
-
-		// tell Godot about our new controller
-		OSIPhone::get_singleton()->joy_connection_changed(
-				joy_id, true, [controller.vendorName UTF8String]);
-
-		// add it to our dictionary, this will retain our controllers
-		ios_joysticks[@(joy_id)] = controller;
-
-		// set our input handler
-		[delegate setControllerInputHandler:controller];
-	} else {
-		printf("Couldn't retrieve new joy id\n");
-	};
-}
-
-- (void)controllerWasConnected:(NSNotification *)notification {
-	// create our dictionary if we don't have one yet
-	if (ios_joysticks == nil) {
-		ios_joysticks = [[NSMutableDictionary alloc] init];
-	};
-
-	// get our controller
-	GCController *controller = (GCController *)notification.object;
-	if (controller == nil) {
-		NSLog(@"No controller attached to notification. Details: %@", notification);
-	} else if ([ios_joysticks.allValues containsObject:controller]) {
-		NSLog(@"Controller is already registered. Details: %@", controller);
-	} else if (frame_count > 1) {
-		_ios_add_joystick(controller, self);
-	} else {
-		if (pending_ios_joysticks == nil)
-			pending_ios_joysticks = [[NSMutableArray alloc] init];
-		[pending_ios_joysticks addObject:controller];
-	};
-};
-
-- (void)controllerWasDisconnected:(NSNotification *)notification {
-	if (ios_joysticks != nil) {
-		// find our joystick, there should be only one in our dictionary
-		GCController *controller = (GCController *)notification.object;
-		NSArray *keys = [ios_joysticks allKeysForObject:controller];
-		for (NSNumber *key in keys) {
-			// tell Godot this joystick is no longer there
-			int joy_id = [key intValue];
-			OSIPhone::get_singleton()->joy_connection_changed(joy_id, false, "");
-
-			// and remove it from our dictionary
-			[ios_joysticks removeObjectForKey:key];
-		};
-	};
-};
-
-- (int)getJoyIdForController:(GCController *)controller {
-	if (ios_joysticks != nil) {
-		// find our joystick, there should be only one in our dictionary
-		NSArray *keys = [ios_joysticks allKeysForObject:controller];
-		for (NSNumber *key in keys) {
-			int joy_id = [key intValue];
-			return joy_id;
-		};
-	};
-
-	return -1;
-};
-
-- (void)setControllerInputHandler:(GCController *)controller {
-	// Hook in the callback handler for the correct gamepad profile.
-	// This is a bit of a weird design choice on Apples part.
-	// You need to select the most capable gamepad profile for the
-	// gamepad attached.
-	if (controller.extendedGamepad != nil) {
-		// The extended gamepad profile has all the input you could possibly find on
-		// a gamepad but will only be active if your gamepad actually has all of
-		// these...
-		controller.extendedGamepad.valueChangedHandler = ^(
-				GCExtendedGamepad *gamepad, GCControllerElement *element) {
-			int joy_id = [self getJoyIdForController:controller];
-
-			if (element == gamepad.buttonA) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_0,
-						gamepad.buttonA.isPressed);
-			} else if (element == gamepad.buttonB) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_1,
-						gamepad.buttonB.isPressed);
-			} else if (element == gamepad.buttonX) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_2,
-						gamepad.buttonX.isPressed);
-			} else if (element == gamepad.buttonY) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_3,
-						gamepad.buttonY.isPressed);
-			} else if (element == gamepad.leftShoulder) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_L,
-						gamepad.leftShoulder.isPressed);
-			} else if (element == gamepad.rightShoulder) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_R,
-						gamepad.rightShoulder.isPressed);
-			} else if (element == gamepad.leftTrigger) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_L2,
-						gamepad.leftTrigger.isPressed);
-			} else if (element == gamepad.rightTrigger) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_R2,
-						gamepad.rightTrigger.isPressed);
-			} else if (element == gamepad.dpad) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_UP,
-						gamepad.dpad.up.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_DOWN,
-						gamepad.dpad.down.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_LEFT,
-						gamepad.dpad.left.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_RIGHT,
-						gamepad.dpad.right.isPressed);
-			};
-
-			InputDefault::JoyAxis jx;
-			jx.min = -1;
-			if (element == gamepad.leftThumbstick) {
-				jx.value = gamepad.leftThumbstick.xAxis.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_LX, jx);
-				jx.value = -gamepad.leftThumbstick.yAxis.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_LY, jx);
-			} else if (element == gamepad.rightThumbstick) {
-				jx.value = gamepad.rightThumbstick.xAxis.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_RX, jx);
-				jx.value = -gamepad.rightThumbstick.yAxis.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_RY, jx);
-			} else if (element == gamepad.leftTrigger) {
-				jx.value = gamepad.leftTrigger.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_L2, jx);
-			} else if (element == gamepad.rightTrigger) {
-				jx.value = gamepad.rightTrigger.value;
-				OSIPhone::get_singleton()->joy_axis(joy_id, JOY_ANALOG_R2, jx);
-			};
-		};
-	} else if (controller.gamepad != nil) {
-		// gamepad is the standard profile with 4 buttons, shoulder buttons and a
-		// D-pad
-		controller.gamepad.valueChangedHandler = ^(GCGamepad *gamepad,
-				GCControllerElement *element) {
-			int joy_id = [self getJoyIdForController:controller];
-
-			if (element == gamepad.buttonA) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_0,
-						gamepad.buttonA.isPressed);
-			} else if (element == gamepad.buttonB) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_1,
-						gamepad.buttonB.isPressed);
-			} else if (element == gamepad.buttonX) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_2,
-						gamepad.buttonX.isPressed);
-			} else if (element == gamepad.buttonY) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_3,
-						gamepad.buttonY.isPressed);
-			} else if (element == gamepad.leftShoulder) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_L,
-						gamepad.leftShoulder.isPressed);
-			} else if (element == gamepad.rightShoulder) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_R,
-						gamepad.rightShoulder.isPressed);
-			} else if (element == gamepad.dpad) {
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_UP,
-						gamepad.dpad.up.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_DOWN,
-						gamepad.dpad.down.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_LEFT,
-						gamepad.dpad.left.isPressed);
-				OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_RIGHT,
-						gamepad.dpad.right.isPressed);
-			};
-		};
-#ifdef ADD_MICRO_GAMEPAD // disabling this for now, only available on iOS 9+,
-		// while we are setting that as the minimum, seems our
-		// build environment doesn't like it
-	} else if (controller.microGamepad != nil) {
-		// micro gamepads were added in OS 9 and feature just 2 buttons and a d-pad
-		controller.microGamepad.valueChangedHandler =
-				^(GCMicroGamepad *gamepad, GCControllerElement *element) {
-					int joy_id = [self getJoyIdForController:controller];
-
-					if (element == gamepad.buttonA) {
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_0,
-								gamepad.buttonA.isPressed);
-					} else if (element == gamepad.buttonX) {
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_BUTTON_2,
-								gamepad.buttonX.isPressed);
-					} else if (element == gamepad.dpad) {
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_UP,
-								gamepad.dpad.up.isPressed);
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_DOWN,
-								gamepad.dpad.down.isPressed);
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_LEFT,
-								gamepad.dpad.left.isPressed);
-						OSIPhone::get_singleton()->joy_button(joy_id, JOY_DPAD_RIGHT,
-								gamepad.dpad.right.isPressed);
-					};
-				};
-#endif
-	};
-
-	///@TODO need to add support for controller.motion which gives us access to
-	/// the orientation of the device (if supported)
-
-	///@TODO need to add support for controllerPausedHandler which should be a
-	/// toggle
-};
-
-- (void)initGameControllers {
-	// get told when controllers connect, this will be called right away for
-	// already connected controllers
-	[[NSNotificationCenter defaultCenter]
-			addObserver:self
-			   selector:@selector(controllerWasConnected:)
-				   name:GCControllerDidConnectNotification
-				 object:nil];
-
-	// get told when controllers disconnect
-	[[NSNotificationCenter defaultCenter]
-			addObserver:self
-			   selector:@selector(controllerWasDisconnected:)
-				   name:GCControllerDidDisconnectNotification
-				 object:nil];
-};
-
-- (void)deinitGameControllers {
-	[[NSNotificationCenter defaultCenter]
-			removeObserver:self
-					  name:GCControllerDidConnectNotification
-					object:nil];
-	[[NSNotificationCenter defaultCenter]
-			removeObserver:self
-					  name:GCControllerDidDisconnectNotification
-					object:nil];
-
-	if (ios_joysticks != nil) {
-		[ios_joysticks dealloc];
-		ios_joysticks = nil;
-	};
-
-	if (pending_ios_joysticks != nil) {
-		[pending_ios_joysticks dealloc];
-		pending_ios_joysticks = nil;
-	};
-};
 
 OS::VideoMode _get_video_mode() {
 	int backingWidth;
@@ -389,10 +131,7 @@ static int frame_count = 0;
 			Main::setup2();
 			++frame_count;
 
-			for (GCController *controller in pending_ios_joysticks) {
-				_ios_add_joystick(controller, self);
-			}
-			[pending_ios_joysticks removeAllObjects];
+			[GamepadManager sharedManager].ready = YES;
 
 			NSDictionary *dict = [[NSBundle mainBundle] infoDictionary];
 			for (NSString *key in dict) {
@@ -564,8 +303,6 @@ static int frame_count = 0;
 			motionInitialised = YES;
 		};
 	};
-
-	[self initGameControllers];
 
 	[[NSNotificationCenter defaultCenter]
 			addObserver:self
