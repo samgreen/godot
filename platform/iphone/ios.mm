aa32e4677ed489148c2f1e8f8a5d9f9bf221e045
Sam Green <samuel.green@instacart.com>
2018-10-06 07:14:35 -0700

    Simplify rating URLs for iOS 9+

--- platform/iphone/ios.mm
+++ platform/iphone/ios.mm
@@ -57,26 +57,10 @@ void iOS::alert(const char *p_alert, const char *p_title) {
 }
 
 String iOS::get_rate_url(int p_app_id) const {
-	String templ = "itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=APP_ID";
-	String templ_iOS7 = "itms-apps://itunes.apple.com/app/idAPP_ID";
-	String templ_iOS8 = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
-
-	//ios7 before
-	String ret = templ;
-
-	if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0 && [[[UIDevice currentDevice] systemVersion] floatValue] < 7.1) {
-		// iOS 7 needs a different templateReviewURL @see https://github.com/arashpayan/appirater/issues/131
-		ret = templ_iOS7;
-	} else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
-		// iOS 8 needs a different templateReviewURL also @see https://github.com/arashpayan/appirater/issues/182
-		ret = templ_iOS8;
-	}
-
-	// ios7 for everything?
-	ret = templ_iOS7.replace("APP_ID", String::num(p_app_id));
-
-	printf("returning rate url %ls\n", ret.c_str());
-	return ret;
+	String rate_url = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+	rate_url = rate_url.replace("APP_ID", String::num(p_app_id));
+	printf("returning rate url %ls\n", rate_url.c_str());
+	return rate_url;
 };
 
 void iOS::show_store_rating_ui() {
